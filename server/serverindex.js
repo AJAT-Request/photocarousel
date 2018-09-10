b354ca3b60a4f045d24a7106e23c755fb8505410
Andrew Mac <macandrew6@gmail.com>
2018-09-10 15:56:33 -0700

    addes a server file listening ton port 8080

---
+++ server/serverindex.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + './client/dist'))
+
+app.get('', (req, res) => {
+  res.send('hello')
+});
+
+var PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`You are listening on port ${PORT}!`)
+})
